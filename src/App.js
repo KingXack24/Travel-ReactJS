9d90270defd6bb4878f2c7d71a855042503acbad
Zakir Hossain <zaknet77@gmail.com>
2022-06-22 08:52:42 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import React from "react";
+import {CssBaseLine, Grid} from @
+import { Header } from "./components/Header/Header";
+import { List } from "./components/List/List";
+import { Map } from "./components/Map/Map";
+
+
+function App() {
+  return (
+    <div className="App">
+     <div>
+       <h1>Travel Advisor</h1>
+     </div>
+    
+    </div>
+  );
+}
+
+export default App;
